8a905af336ffda34058ff453b49ef9218317e698
Alex Yaseen <alex@parabola.io>
2025-09-14 15:49:59 -0400

    Electron: prevent duplicate logs by clearing IPC listeners before each run; expose clearMigrationListeners in preload and call it from renderer

--- electron/preload.js
+++ electron/preload.js
@@ -4,6 +4,11 @@ contextBridge.exposeInMainWorld('electronAPI', {
   saveSecrets: (frontToken, googleCredentialsJson) => ipcRenderer.invoke('save-secrets', { frontToken, googleCredentialsJson }),
   runMigration: (opts) => ipcRenderer.invoke('run-migration', opts),
   cancelMigration: () => ipcRenderer.invoke('cancel-migration'),
+  clearMigrationListeners: () => {
+    ipcRenderer.removeAllListeners('migration-data');
+    ipcRenderer.removeAllListeners('migration-end');
+    ipcRenderer.removeAllListeners('migration-error');
+  },
   onMigrationData: (callback) => ipcRenderer.on('migration-data', (_e, chunk) => callback(chunk)),
   onMigrationEnd: (callback) => ipcRenderer.on('migration-end', (_e, payload) => callback(payload)),
   onMigrationError: (callback) => ipcRenderer.on('migration-error', (_e, payload) => callback(payload)),
